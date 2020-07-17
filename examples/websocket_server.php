d87f25890c251bb39503cb705c34f011d8bbfdab
zyfei <943901167@qq.com>
2020-07-17 14:07:23 +0800

    Update websocket_server.php

--- examples/websocket_server.php
+++ examples/websocket_server.php
@@ -7,19 +7,15 @@ Warriorman\Worker::rename(); // 将Workerman改为Workerman
 Warriorman\Runtime::enableCoroutine(); // hook相关函数
                                        
 // 创建一个Worker监听2345端口，使用http协议通讯
-$http_worker = new Worker("tcp://0.0.0.0:2345");
+$ws_worker = new Worker("tcp://0.0.0.0:2345");
 
 // 启动4个进程对外提供服务
-$http_worker->count = 4;
+$ws_worker->count = 4;
 // 设置协议
-$http_worker->protocol = "\Workerman\Protocols\Websocket";
-
-$http_worker->onConnect = function ($connection) {
-	echo "new connection id {$connection->id} \n";
-};
+$ws_worker->protocol = "\Workerman\Protocols\Websocket";
 
 // 接收到浏览器发送的数据时回复hello world给浏览器
-$http_worker->onMessage = function ($connection, $data) {
+$ws_worker->onMessage = function ($connection, $data) {
 	// 向浏览器发送hello world
 	$connection->send('hello world');
 };
